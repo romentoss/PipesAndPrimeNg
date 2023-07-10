7005d363ccc7a44ee5ba9da6afd9bf4475bb5bcc
Romentos <padilla.naranja@gmail.com>
2023-07-10 14:31:28 +0200

    first commit

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import {MenuModule} from 'primeng/menu';
+import {MenubarModule} from 'primeng/menubar'
+
+@NgModule({
+  imports: [],
+  exports: [MenuModule,MenubarModule],
+
+})
+export class PrimeNgModule { }
